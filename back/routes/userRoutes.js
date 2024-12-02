0d7510917c79dea32492791bff60ee40604bc7a8
AldanaV <aldanav.salvatierra@gmail.com>
2024-12-02 19:21:29 -0300

    manejo de sesiones en back y front

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -25,13 +25,43 @@ router.post('/register', async(req, res) => {
 
 //Ruta para iniciar sesion
 router.post('/login', async(req,res) => {
-    
-})
+    const {username, password} = req.body;
+    try{
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(401).send('Usuario no encontrado.');
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            console.error('Contraseña incorrecta para el usuario: ',username);
+            return res.status(404).send('Contraseña incorrecta.');
+        }
+        
+        req.session.user = user.username;
+        res.send(`Usuario ${user.username} ha iniciado sesión`);
+    }catch(err){
+        res.status(500).send('Error en el servidor.');
+    }
+});
 
 
 //Ruta para cerrar sesion
-
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if(err){
+            return res.status(500).send('Error al cerrar sesión.');
+        }
+        res.send('Sesión cerrada.');
+    });
+});
 
 //Ruta de contenido protegido
+router.get('/privada', (req, res) => {
+    if(req.session.user){
+        res.send(`Bienvenido, ${req.session.user}`);
+    } else{
+        res.status(401).send('Entrada no autorizada.');
+    }
+});
 
 module.exports = router;
---
+++ front/src/componentes/api/AuthContext.jsx
@@ -0,0 +1,27 @@
+//Gestiona toda la app
+import React, {createContext, useState, useContext, useEffect} from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ Children}) => { //envuelve la app, se van a poder definir las sesiones que necesitan esta autentificacion
+    const [isAuthenticaded, setIsAuthenticaded] = useState(() => {
+        return localStorage.getItem('isAuthenticaded') === 'true';
+    });
+
+    const login = () => {
+        setIsAuthenticaded(true);
+        localStorage.setItem('isAuthenticaded', 'true');
+    };
+
+    const logout = () => {
+        setIsAuthenticaded(false);
+        localStorage.removeItem('isAuthenticaded'); 
+    }
+    return(
+        <AuthContext.Provider value = {{ isAuthenticaded, login, logout}}>
+        {Children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
